1df187b2726c81c0cf9f270fa00609c8bedba329
Nathaniel Christian <nathaniel.christian@gmail.com>
2026-02-11 20:33:18 +0700

    new board func

--- src/board.go
+++ src/board.go
@@ -6,8 +6,22 @@ type Board struct {
 	queen [][]bool
 }
 
-func newBoard(colors [][]rune) *Board
-func (b *Board) isValid() bool
+func newBoard(colorMatrix [][]rune) *Board {
+	size := len(colorMatrix)
+
+	queenMatrix := make([][]bool, size)
+	for i := 0; i < size; i++ {
+		queenMatrix[i] = make([]bool, size)
+	}
+
+	return &Board {
+		size: size,
+		color: colorMatrix,
+		queen: queenMatrix,
+	}
+}
+
+func (b *Board) isValid() bool   // every color must appear once, number of color == board size/length
 func (b *Board) isPlaceable(row, col int)
 func (b *Board) addQueen(row, col int)
 func (b *Board) rmvQueen(row, col int)
