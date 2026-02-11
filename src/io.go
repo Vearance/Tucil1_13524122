05a7be66557ac919dc7362f887ae1ee3b503c531
Nathaniel Christian <nathaniel.christian@gmail.com>
2026-02-11 20:35:24 +0700

    constructBoard

--- src/io.go
+++ src/io.go
@@ -1,6 +1,32 @@
 package main
 
-func constructBoard(filename string) (*Board, error)
+import (
+	"fmt"
+	"bufio"
+	"os"
+)
+
+func constructBoard(filename string) (*Board, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text() // current line as string
+		fmt.Println(line) // TODO: change this to put into board
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return //returns (newBoard(...), nil)
+}
+
 func saveBoard(b *Board, filename string) error
 
 func printBoard(b *Board)
